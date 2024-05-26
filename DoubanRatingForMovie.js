5b2d556a7f206f9dbe6331fab57398f2e60866f0
ciphersaw <ciphersaw@gmail.com>
2024-05-26 11:01:04 +0800

    feat(olevod): support for the new site of Olevod
    1) Because the DOM elements are loaded by AJAX, the title needs to be resolved periodically.

--- DoubanRatingForMovie.js
+++ DoubanRatingForMovie.js
@@ -2,11 +2,13 @@
 // @name         DoubanRatingForMovie
 // @name:zh-CN   在线电影添加豆瓣评分
 // @namespace    https://github.com/ciphersaw/DoubanRatingForMovie
-// @version      1.0.2
+// @version      1.0.3
 // @description  Display Douban rating for online movies.
 // @description:zh-CN  在主流电影网站上显示豆瓣评分。
 // @author       CipherSaw
 // @match        *://*.olehdtv.com/index.php*
+// @match        *://*.olevod.com/details*
+// @match        *://*.olevod.com/player/vod/*
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @connect      douban.com
@@ -61,9 +63,12 @@ const DOUBAN_RATING_API = 'https://www.douban.com/search?cat=1002&q=';
     const host = location.hostname;
     if (host === 'www.olehdtv.com') {
         OLEHDTV_setRating();
+    } else if (host === 'www.olevod.com') {
+        OLEVOD_setRating();
     }
 })();
 
+// ==OLEHDTV==
 function OLEHDTV_setRating() {
     const id = OLEHDTV_getID();
     const title = OLEHDTV_getTitle();
@@ -82,9 +87,9 @@ function OLEHDTV_getID() {
 }
 
 function OLEHDTV_getTitle() {
-    let title = $('h2.title').clone();
-    title.children().remove();
-    return title.text().trim().replace(/【.*】$/, ''); // Remove the annotated suffix of title
+    let clone = $('h2.title').clone();
+    clone.children().remove();
+    return clone.text().trim().replace(/【.*】$/, ''); // Remove the annotated suffix of title
 }
 
 function OLEHDTV_setMainRating(ratingNums, url) {
@@ -95,8 +100,8 @@ function OLEHDTV_setMainRating(ratingNums, url) {
         ratingObj.append(doubanLink);
     } else if (OLEHDTV_isPlayPage()) {
         let ratingObj = $('.play_text .nstem');
-        const replacedText = ratingObj.html().replace('豆瓣评分：', '');
-        ratingObj.html(replacedText);
+        const replacedHTML = ratingObj.html().replace('豆瓣评分：', '');
+        ratingObj.html(replacedHTML);
         ratingObj.append(doubanLink);
     }
 }
@@ -109,12 +114,103 @@ function OLEHDTV_isPlayPage() {
     return /.+\/vod\/play\/id\/\d+.*/.test(location.href);
 }
 
+// ==OLEVOD==
+async function OLEVOD_setRating() {
+    const id = OLEVOD_getID();
+    let title = '';
+    try {
+        title = await OLEVOD_waitForTitle(1000, 10);
+    } catch (error) {
+        logger.error(`OLEVOD_waitForTitle: id=${id} error=${error}`);
+        return;
+    }
+    getDoubanRating(`olevod_${id}`, title)
+        .then(data => {
+            OLEVOD_setMainRating(data.ratingNums, data.url);
+        })
+        .catch(err => {
+            OLEVOD_setMainRating("N/A", DOUBAN_RATING_API + title);
+        });
+}
+
+function OLEVOD_getID() {
+    const id = /\d{1}-\d{5}/.exec(location.href);
+    return id ? id[0] : 0;
+}
+
+function OLEVOD_waitForTitle(delay, iterations) {
+    let selector = '';
+    if (OLEVOD_isDetailPage()) {
+        selector = ".pc-container .info .title";
+    } else if (OLEVOD_isPlayPage()) {
+        selector = ".el-tabs__content .tab-label";
+    }
+    return new Promise((resolve, reject) => {
+        let count = 0;
+        const intervalID = setInterval(() => {
+            count++;
+            if (count === iterations) {
+                const error = new Error(`ResolveError: title is not found and iterations have reached the maximum`);
+                clearInterval(intervalID);
+                reject(error);
+            }
+            const obj = $(selector);
+            if (obj) {
+                const title = OLEVOD_resolveTitle(obj);
+                if (title !== "") {
+                    clearInterval(intervalID);
+                    resolve(title);
+                }
+            }
+        }, delay);
+    });
+}
+
+function OLEVOD_resolveTitle(obj) {
+    const suffixRegex = /【.*】$/; // Remove the annotated suffix of title
+    if (OLEVOD_isDetailPage()) {
+        return obj.text().trim().replace(suffixRegex, '');
+    } else if (OLEVOD_isPlayPage()) {
+        const clone = obj.clone();
+        clone.children().remove();
+        return clone.text().trim().replace(suffixRegex, '');
+    }
+}
+
+function OLEVOD_setMainRating(ratingNums, url) {
+    if (OLEVOD_isDetailPage()) {
+        let ratingObj = $('.pc-container .info .label:first-child');
+        ratingObj.before(`<span class="label"><a href="${url}" target="_blank" style="color: white">豆瓣评分：${ratingNums}</a></span>`);
+    } else if (OLEVOD_isPlayPage()) {
+        let ratingObj = $('#pane-first .tab-label .wes');
+        const clone = ratingObj.clone();
+        clone.children().remove();
+        const originalText = clone.text().trim();
+        const array = originalText.split(/ +/);
+        if (array.length === 2) {
+            const revisedText = `${array[0]} <a href="${url}" target="_blank" style="color: #798499">豆瓣${ratingNums}</a>/${array[1]}`;
+            const replacedHTML = ratingObj.html().replace(originalText, revisedText);
+            ratingObj.html(replacedHTML);
+        }
+
+    }
+}
+
+function OLEVOD_isDetailPage() {
+    return /.+\/details-\d{1}-\d{5}\.html/.test(location.href);
+}
+
+function OLEVOD_isPlayPage() {
+    return /.+\/player\/vod\/\d{1}-\d{5}-\d{1}\.html/.test(location.href);
+}
+
+// ==COMMON==
 function clearExpiredCache() {
     const t = GM_getValue('clear_time');
     if (!t || !isValidTime(new Date(t), PERIOD_OF_CLEARING_CACHE)) {
         logger.info(`clearExpiredCache: clear_time=${t}`);
         const idList = GM_listValues();
-        idList.forEach(function(id) {
+        idList.forEach(function (id) {
             // Delete the expired IDs periodically
             const data = GM_getValue(id);
             if (data.uptime && !isValidTime(new Date(data.uptime), TERM_OF_VALID_CACHE)) {
@@ -142,14 +238,15 @@ async function getDoubanRating(key, title) {
             "onload": (r) => {
                 const response = $($.parseHTML(r.response));
                 if (r.status !== 200) {
-                    reject(new Error(`StatusError: response status is ${r.status} and message is ${r.statusText}`));
+                    const error = new Error(`StatusError: response status is ${r.status} and message is ${r.statusText}`);
+                    reject(error);
                 } else {
                     try {
                         let data = resolveDoubanRatingResult(url, response);
                         logger.info(`getDoubanRating: title=${title} rating=${data.ratingNums}`);
                         resolve(data);
                     } catch (error) {
-                        logger.error(`getDoubanRating: title=${title} error=${err}`);
+                        logger.error(`getDoubanRating: title=${title} error=${error}`);
                         reject(error);
                     }
                 }
@@ -180,7 +277,7 @@ function cacheDoubanRatingData(key, ratingData) {
 function resolveDoubanRatingResult(searchURL, data) {
     const s = data.find('.result-list .result:first-child');
     if (s.length === 0) {
-        throw Error("ResolveError: search result not found");
+        throw Error("ResolveError: search result is not found");
     }
     const ratingNums = s.find('.rating_nums').text() || '暂无评分';
     const doubanLink = s.find('.content .title a').attr('href') || '';
